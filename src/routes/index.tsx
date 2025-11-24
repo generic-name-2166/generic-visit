6aadd928caa030f935e5b65bbc7f9927bb5c75e6
generic-name-2166 <kirbesson277@mail.ru>
2025-11-24 15:38:42 +0300

    feat: more fingerprinting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,38 @@
 import { createFileRoute } from "@tanstack/solid-router";
 import { createClientOnlyFn, json, JsonResponse } from "@tanstack/solid-start";
-import { createResource, type JSX } from "solid-js";
+import { createResource, Suspense, type JSX } from "solid-js";
 
 import type { CloudflareContext } from "../server.ts";
 
 interface ClientFingerprint {
-  timezoneOffset: number;
+  readonly timezoneOffset: number;
+  readonly timezone: string;
+  readonly width: number;
+  readonly height: number;
+  readonly colorDepth: number;
+  readonly platform: string;
+  readonly hardwareConcurrency: number;
 }
 
 type Visit = CloudflareContext & ClientFingerprint;
 
 const query = createClientOnlyFn(async (): Promise<Visit[]> => {
   // https://coveryourtracks.eff.org/
-  const body = { timezoneOffset: new Date().getTimezoneOffset() };
+  const timezone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const width: number = window.screen.width;
+  const height: number = window.screen.height;
+  const colorDepth: number = window.screen.colorDepth;
+  const platform: string = navigator.platform;
+  const hardwareConcurrency = navigator.hardwareConcurrency;
+  const body: ClientFingerprint = {
+    timezoneOffset: new Date().getTimezoneOffset(),
+    timezone,
+    width,
+    height,
+    colorDepth,
+    platform,
+    hardwareConcurrency,
+  };
   const resp = await fetch(window.location.href, {
     method: "POST",
     body: JSON.stringify(body),
@@ -25,7 +45,11 @@ const query = createClientOnlyFn(async (): Promise<Visit[]> => {
 function Index(): JSX.Element {
   const [state] = createResource(query);
 
-  return <pre>{JSON.stringify(state(), null, 2)}</pre>;
+  return (
+    <Suspense>
+      <pre>{JSON.stringify(state(), null, 2)}</pre>
+    </Suspense>
+  );
 }
 
 export const Route = createFileRoute("/")({
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ export interface CloudflareContext {
   country?: string;
   city?: string;
   postalCode?: string;
+  language: string;
+  encoding?: string;
 }
 
 declare module "@tanstack/solid-start" {
@@ -19,14 +21,32 @@ declare module "@tanstack/solid-start" {
 
 export default createServerEntry({
   fetch(request) {
-    const date = new Date(request.headers.get("date") ?? 0);
+    const date = new Date(request.headers.get("date") ?? Date.now());
     // accept header is useless on POST request as it is always "*/*"
     const userAgent = request.headers.get("user-agent") ?? undefined;
     const ip = request.headers.get("cf-connecting-ip") ?? undefined;
     const country = request.cf?.country as string | undefined;
     const city = request.cf?.city as string | undefined;
     const postalCode = request.cf?.postalCode as string | undefined;
-    const context = { date, userAgent, ip, country, city, postalCode };
+    // en-US,en;q=0.7  https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Accept-Language
+    // only the first language is typically used for privacy
+    const language: string =
+      (request.headers.get("accept-language") ?? "")
+        .split(",")
+        .at(0)
+        ?.split(";")
+        .at(0) ?? "";
+    const encoding = request.headers.get("accept-encoding") ?? undefined;
+    const context = {
+      date,
+      userAgent,
+      ip,
+      country,
+      city,
+      postalCode,
+      language,
+      encoding,
+    };
     return handler.fetch(request, {
       context,
     });
