0708c45b534fce85c6349e36ca42f24c8e28636b
generic-name-2166 <kirbesson277@mail.ru>
2025-11-26 00:27:27 +0300

    fix: css modules none to just fouc

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,52 @@
 import { createFileRoute } from "@tanstack/solid-router";
-import { createClientOnlyFn, json, JsonResponse } from "@tanstack/solid-start";
+import {
+  createIsomorphicFn,
+  json,
+  JsonResponse,
+} from "@tanstack/solid-start";
 import { createResource, type JSX } from "solid-js";
 
 import Main, { type ClientFingerprint, type Visit } from "../comp/Main.tsx";
 
-const query = createClientOnlyFn(async (): Promise<Visit[]> => {
-  // https://coveryourtracks.eff.org/
-  const timezone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const width: number = window.screen.width;
-  const height: number = window.screen.height;
-  const colorDepth: number = window.screen.colorDepth;
-  const platform: string = navigator.platform;
-  const hardwareConcurrency = navigator.hardwareConcurrency;
-  const body: ClientFingerprint = {
-    timezoneOffset: new Date().getTimezoneOffset(),
-    timezone,
-    width,
-    height,
-    colorDepth,
-    platform,
-    hardwareConcurrency,
-  };
-  const resp = await fetch(window.location.href, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" },
-  });
-  const data: Visit[] = await resp.json();
-  return data.map((visit) => ({ ...visit, date: new Date(visit.date) }));
-});
+const query = createIsomorphicFn()
+  .client(async (): Promise<Visit[]> => {
+    // https://coveryourtracks.eff.org/
+    const timezone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const width: number = window.screen.width;
+    const height: number = window.screen.height;
+    const colorDepth: number = window.screen.colorDepth;
+    const platform: string = navigator.platform;
+    const hardwareConcurrency = navigator.hardwareConcurrency;
+    const body: ClientFingerprint = {
+      timezoneOffset: new Date().getTimezoneOffset(),
+      timezone,
+      width,
+      height,
+      colorDepth,
+      platform,
+      hardwareConcurrency,
+    };
+    const resp = await fetch(window.location.href, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data: Visit[] = await resp.json();
+    return data.map((visit) => ({ ...visit, date: new Date(visit.date) }));
+  })
+  .server((): Visit[] => [
+    {
+      date: new Date(),
+      language: "",
+      timezoneOffset: 0,
+      timezone: "",
+      width: 0,
+      height: 0,
+      colorDepth: 0,
+      platform: "",
+      hardwareConcurrency: 0,
+    } satisfies Visit,
+  ]);
 
 function Index(): JSX.Element {
   const [state] = createResource(query);
@@ -43,8 +61,9 @@ function Index(): JSX.Element {
 
 export const Route = createFileRoute("/")({
   component: Index,
-  // TODO: make this ssr more granular
-  ssr: false,
+  // unfortunate FOUC with CSS modules with SSR, no CSS at all without
+  // https://github.com/TanStack/router/issues/3023
+  ssr: true,
   server: {
     handlers: {
       POST: async ({ request, context }): Promise<JsonResponse<Visit[]>> => {
